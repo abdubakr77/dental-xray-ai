45b469d1b58b84053dca2f2e194c1acc7153aa2f
abdubakr77 <abdubakora1232@gmail.com>
2026-07-05 19:36:43 +0300

    feat: add a multilabel stratified train, validation, and test splitting utility

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import numpy as np
-import os
+from sklearn.preprocessing import MultiLabelBinarizer
+import pandas as pd
+
+# pip install iterative-stratification
+from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 def compute_iou(box1, box2):
     # Must boxes format are: [x, y, w, h]
@@ -52,3 +53,81 @@ def find_duplicate_boxes(df, iou_threshold=0.9):
     print(f"Number of duplicate Boxes: {len(duplicates)}")
     
     return pd.DataFrame(duplicates)
+
+
+
+
+def multilabel_train_val_test_split(df,y,test_size=0.2,apply_check = False):
+
+
+    grouped = (
+        df.groupby("File_Name")[y]
+        .apply(list)
+        .reset_index()
+    )
+
+
+    mlb = MultiLabelBinarizer()
+
+    Y = mlb.fit_transform(grouped[y])
+
+
+    msss = MultilabelStratifiedShuffleSplit(
+        n_splits=1,
+        test_size=test_size,
+        random_state=42
+    )
+
+    train_idx, temp_idx = next(msss.split(grouped["File_Name"], Y))
+
+    train_images = grouped.iloc[train_idx]
+    temp_images = grouped.iloc[temp_idx]
+
+
+
+    Y_temp = Y[temp_idx]
+
+    msss2 = MultilabelStratifiedShuffleSplit(
+        n_splits=1,
+        test_size=0.5,
+        random_state=42
+    )
+
+    val_idx, test_idx = next(
+        msss2.split(temp_images["File_Name"], Y_temp)
+    )
+
+    val_images = temp_images.iloc[val_idx]
+    test_images = temp_images.iloc[test_idx]
+
+
+
+    train_df = df[df["File_Name"].isin(train_images["File_Name"])].reset_index(drop=True)
+
+    val_df = df[df["File_Name"].isin(val_images["File_Name"])].reset_index(drop=True)
+
+    test_df = df[df["File_Name"].isin(test_images["File_Name"])].reset_index(drop=True)
+
+    if apply_check:
+
+        print("Train Images :", train_images.shape[0])
+        print("Val Images   :", val_images.shape[0])
+        print("Test Images  :", test_images.shape[0])
+
+        print()
+
+        print("Train Rows :", len(train_df))
+        print("Val Rows   :", len(val_df))
+        print("Test Rows  :", len(test_df))
+
+        train_files = set(train_df["File_Name"])
+        val_files = set(val_df["File_Name"])
+        test_files = set(test_df["File_Name"])
+
+        assert train_files.isdisjoint(val_files)
+        assert train_files.isdisjoint(test_files)
+        assert val_files.isdisjoint(test_files)
+
+        print("Perfect! No Data Leakage Found.")
+
+    return train_df, val_df , test_df
