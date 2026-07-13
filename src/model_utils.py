e3356eaf9b4a5793cfe11efc7e4e7de03a3bf0b2
abdubakr77 <abdubakora1232@gmail.com>
2026-07-13 15:19:29 +0300

    feat: add a utility for YOLO inference and prediction visualization

---
+++ src/model_utils.py
@@ -0,0 +1,33 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def predict(yolo_model, image, conf_filter=0.5, custom_draw_box:function=None, crop_output_image:str=False, save_output:bool=False, save_dir:str=None):
+    outputs = yolo_model.predict(image,conf=conf_filter)
+
+    output = outputs[0]
+    boxes = output.boxes
+    names = output.names
+
+    _,ax = plt.subplots(1,2,figsize=(12,8))
+    for i in range(len(boxes)):
+        confidence = boxes[i].conf
+        coordinates = boxes[i].xyxy
+        cls_name = names[boxes[i].cls.numpy().item()]
+
+        print(f'Class Name: {cls_name}')
+        print(f'Coordinates: {coordinates}')
+        print(f'Confidence: %{(confidence * 100):.4}')
+        
+    ax[0].set_title('Original Image')
+    ax[0].imshow(image)
+    ax[0].yticks([])
+    ax[0].xticks([])
+
+    ax[1].set_title('Object Detected')
+    ax[1].imshow(output.plot()[:,:,::-1])
+    ax[1].yticks([])
+    ax[1].xticks([])
+    
+    plt.tight_layout()
+    plt.show()
