5c0b61b62b07333d608d9bab878ed970c09cf3c0
abdubakr77 <abdubakora1232@gmail.com>
2026-07-16 23:19:53 +0300

    fix: skip augmented file checks when running debug mode

--- src/utils.py
+++ src/utils.py
@@ -362,24 +362,7 @@ def apply_smart_aug(data_yaml,aug_config,apply_debug=False):
     
     all_files_no_ext = [item.split('.')[0] for item in os.listdir(imgs_path)]
 
-    if 'aug' in ', '.join(all_files_no_ext):
-        print(f"Warning: Found {all_files_no_ext.count('aug')} images are already augmented!")
-        if input(f'Do you want to delete all {all_files_no_ext.count('aug')} augmented images before processing on the images? - (y or n): ').lower().strip() == 'y':
-            
-            for fname in all_files_no_ext:
-                if fname.count('aug') >= 1:
-                    img_path = os.path.join(imgs_path, fname+'.png')
-                    label_path = os.path.join(labels_path, fname+'.txt')
-                    try:
-                        os.remove(img_path)
-                        os.remove(label_path)
-                    except:
-                        pass
-                    print(f'{fname} deleted!')
-            
-            clear_output()
         
-
     if apply_debug:
         rand_fname = np.random.choice(all_files_no_ext)
         image,bboxes,class_labels = read_image_and_label(rand_fname,data_yaml)
@@ -394,6 +377,24 @@ def apply_smart_aug(data_yaml,aug_config,apply_debug=False):
                         debugging=True)
 
     else:
+        if 'aug' in ', '.join(all_files_no_ext):
+            print(f"Warning: Found {all_files_no_ext.count('aug')} images are already augmented!")
+            if input(f'Do you want to delete all {all_files_no_ext.count('aug')} augmented images before processing on the images? - (y or n): ').lower().strip() == 'y':
+                
+                for fname in all_files_no_ext:
+                    if fname.count('aug') >= 1:
+                        img_path = os.path.join(imgs_path, fname+'.png')
+                        label_path = os.path.join(labels_path, fname+'.txt')
+                        try:
+                            os.remove(img_path)
+                            os.remove(label_path)
+                        except:
+                            pass
+                        print(f'{fname} deleted!')
+                
+                clear_output()
+
+        
         for fname in tqdm(all_files_no_ext,desc=f'Augmenting Images Now...'):
 
             image,bboxes,class_labels = read_image_and_label(fname,data_yaml)
